d2607f30e99d6203f6a88f45baf5b9d6ab8e9247
Sam Brown <jumpbro@hotmail.com>
2022-08-31 18:29:25 +1000

    Completed

---
+++ interactive-rating-component-main/main.js
@@ -0,0 +1,17 @@
+let rating = 0
+
+let ratingCircles = document.querySelectorAll(".ratingCircle")
+ratingCircles.forEach(circle => circle.addEventListener('click', (e) => {
+    ratingCircles.forEach(c => c.classList.remove('active'))
+    rating = e.target.innerText
+    document.querySelector("#ratingSelection").innerText = rating
+    e.target.classList.toggle('active')
+}))
+
+document.getElementById("submitButton").addEventListener('click', () => {
+    if(rating != 0){
+        document.getElementById("ratingStateCard").classList.add('hidden')
+        document.getElementById("thankYouStateCard").classList.remove('hidden')
+    }
+
+})

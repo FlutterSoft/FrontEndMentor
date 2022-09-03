e123e94e6fbdb160dde590143cd97a08e30d3f03
Sam Brown <jumpbro@hotmail.com>
2022-09-03 13:32:27 +1000

    nft-preview

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -1,6 +1,7 @@
 let rating = 0
 
 let ratingCircles = document.querySelectorAll(".ratingCircle")
+
 ratingCircles.forEach(circle => circle.addEventListener('click', (e) => {
     ratingCircles.forEach(c => c.classList.remove('active'))
     rating = e.target.innerText

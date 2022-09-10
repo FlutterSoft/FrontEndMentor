146a84858f32d13ca3787fec6212dc5d35a5e15c
Sam Brown <jumpbro@hotmail.com>
2022-09-10 15:51:05 +1000

    Finished accordion

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -2,9 +2,13 @@ let accordion = document.getElementsByClassName("accordion")
 
 for(let i = 0; i < accordion.length; i++){
     accordion[i].addEventListener("click", function(){
-        // for(let j = 0; j < accordion.length; j++){
-        //     accordion[j].nextElementSibling.style.maxHeight = null
-        // }
+        for(let j = 0; j < accordion.length; j++){
+            if(accordion[j] != this){
+                accordion[j].nextElementSibling.style.maxHeight = null
+                accordion[j].classList.remove('active')
+            }
+
+        }
 
         this.classList.toggle("active")
         let panel = this.nextElementSibling

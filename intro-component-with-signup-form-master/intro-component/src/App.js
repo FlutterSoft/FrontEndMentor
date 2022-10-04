bdc349c86659097cabeba61be68dc2a98cd1bc06
Sam Brown <jumpbro@hotmail.com>
2022-10-05 08:05:16 +1000

    Finished build

--- intro-component-with-signup-form-master/intro-component/src/App.js
+++ intro-component-with-signup-form-master/intro-component/src/App.js
@@ -11,13 +11,48 @@ function TextSection({title, paragraph}){
   )
 }
 
-function Button({text, className}){
+function Button({text, className, onClick}){
   return(
-    <button className={className} onClick={handleClick}>{text}</button>
+    <button className={className} onClick={onClick}>{text}</button>
   )
 }
 function handleClick(e){
-  console.log(document.querySelector('.Form').password.validity)
+  e.preventDefault()
+  document.querySelectorAll('input').forEach(el => {
+    if(!el.validity.valid){
+      el.classList.add('invalid')
+    }
+    else{
+      el.classList.remove('invalid')
+    }
+    if(el.validity.valueMissing){
+      el.nextSibling.innerHTML=`${el.placeholder} cannot be empty`
+    }
+    else if(el.name == "email" && !el.validity.valueMissing && !el.validity.valid){
+      el.nextSibling.innerHTML='Invalid Email Address'
+    }
+    else{
+      el.nextSibling.innerHTML=''
+    }
+
+    if(el.name == "password" && !el.validity.valid && !el.validity.valueMissing){
+      document.querySelector('.passwordInvalid').classList.remove('hidden')
+    }
+    else{
+      document.querySelector('.passwordInvalid').classList.add('hidden')
+    }
+  })
+  const arr = [...document.querySelectorAll('input')]
+  if(arr.filter(el => el.validity.valid == true).length == 4){
+    resetForm()
+  }
+}
+
+function resetForm(){
+  document.querySelectorAll('input').forEach(el =>{
+    el.value = ''
+  })
+  document.querySelector('.SuccessMessage').classList.remove('hidden')
 }
 
 function Form(){
@@ -28,14 +63,36 @@ function Form(){
         name="firstName" 
         placeholder="First Name" 
         required
-          
-      >
-        {/* <img src=""></img> */}
-      </input>
-      <input type="text" name="lastName" placeholder="Last Name" required/>
-      <input type="email" name="email" placeholder="Email Address" required/>
-      <input type="text" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" placeholder="Password" required/>
+      /> 
+      <span className='InputEmpty'></span>
+
+      <input 
+        type="text" 
+        name="lastName" 
+        placeholder="Last Name" 
+        required
+      />
+      <span className='InputEmpty'></span>
+
+      <input 
+        type="email" 
+        name="email" 
+        placeholder="Email Address" 
+        required
+      />
+      <span className='InputEmpty'></span>
+
+      <input 
+        type="text" 
+        name="password" 
+        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" 
+        placeholder="Password" 
+        required
+      />
+      <span className='InputEmpty'></span>
+      <span className="passwordInvalid hidden">Password must be at least 6 characters long and contain an uppercase letter, a lowercase letter, a number and a special symbol.</span>
       <Button className="PrimaryButton" text="CLAIM YOUR FREE TRIAL" onClick={handleClick} />
+      <span className="SuccessMessage hidden">Submitted Successfully</span>
       <span className="TermsSpan">By clicking the button, you are agreeting to our <a href="#">Terms and Services.</a></span>
     </form>
   )

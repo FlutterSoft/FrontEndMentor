8c737a79d90aa51a96496fbcf2b45de4d8d8f479
Sam Brown <jumpbro@hotmail.com>
2022-10-04 20:06:31 +1000

    css

--- intro-component-with-signup-form-master/intro-component/src/App.js
+++ intro-component-with-signup-form-master/intro-component/src/App.js
@@ -13,19 +13,30 @@ function TextSection({title, paragraph}){
 
 function Button({text, className}){
   return(
-    <button className={className}>{text}</button>
+    <button className={className} onClick={handleClick}>{text}</button>
   )
+}
+function handleClick(e){
+  console.log(document.querySelector('.Form').password.validity)
 }
 
 function Form(){
   return(
     <form className="Form">
-      <input type="text" placeholder="First Name"/>
-      <input type="text" placeholder="Last Name"/>
-      <input type="email" placeholder="Email Address"/>
-      <input type="text" placeholder="Password"/>
-      <Button className="PrimaryButton" text="CLAIM YOUR FREE TRIAL" />
-      <span class="TermsSpan">By clicking the button, you are agreeting to our <a href="#">Terms and Services.</a></span>
+      <input 
+        type="text" 
+        name="firstName" 
+        placeholder="First Name" 
+        required
+          
+      >
+        {/* <img src=""></img> */}
+      </input>
+      <input type="text" name="lastName" placeholder="Last Name" required/>
+      <input type="email" name="email" placeholder="Email Address" required/>
+      <input type="text" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" placeholder="Password" required/>
+      <Button className="PrimaryButton" text="CLAIM YOUR FREE TRIAL" onClick={handleClick} />
+      <span className="TermsSpan">By clicking the button, you are agreeting to our <a href="#">Terms and Services.</a></span>
     </form>
   )
 }

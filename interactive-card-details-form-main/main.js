5f3859de1476f0c2cb31144e968faa2dbda0b514
Sam Brown <jumpbro@hotmail.com>
2022-10-19 19:23:25 +1000

    Mobile css made and started on javascript

--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -1,1 +1,18 @@
-console.log('working')
+const cardNameInput = document.querySelector("#name-input")
+const cardNumberInput = document.querySelector("#number-input")
+const cardMonthInput = document.querySelector("#month-expiry-input")
+const cardYearInput = document.querySelector("#year-expiry-input")
+const cardCvcInput = document.querySelector("#cvc-input")
+
+const cardNumber = document.querySelector("#card-number")
+
+document.querySelector("#number-input").addEventListener('change', function(){
+    cardNumber.innerHTML = this.value
+})
+
+
+document.querySelector("#form").addEventListener('submit', function(e){
+    // e.preventDefault()
+    console.log('yoo')
+})
+

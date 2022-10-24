61cffe21a88ab9f91657e5deb0b293d792f6ed1f
Sam Brown <jumpbro@hotmail.com>
2022-10-24 19:37:47 +1000

    Added inputs to card with some verification

--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -5,6 +5,9 @@ const cardYearInput = document.querySelector("#year-expiry-input")
 const cardCvcInput = document.querySelector("#cvc-input")
 
 const cardNumber = document.querySelector("#card-number")
+const cardName = document.querySelector("#card-name")
+const cardExpiry = document.querySelector("#card-expiry")
+const cardCvc = document.querySelector("#card-cvc")
 
 document.querySelector("#number-input").addEventListener('change', function(){
     cardNumber.innerHTML = this.value.slice(0,4) + ' ' + this.value.slice(4,8) + ' ' +
@@ -12,6 +15,22 @@ document.querySelector("#number-input").addEventListener('change', function(){
     this.value.slice(12, 16)
 })
 
+document.querySelector("#cvc-input").addEventListener('change', function(){
+    if(this.value > 0 && this.value < 999 && this.value.length == 3)
+    cardCvc.innerHTML = this.value
+})
+
+document.querySelector("#name-input").addEventListener('change', function(){
+    cardName.innerHTML = this.value.toUpperCase()
+})
+document.querySelector("#month-expiry-input").addEventListener('change', addExpiry)
+document.querySelector("#year-expiry-input").addEventListener('change', addExpiry)
+function addExpiry(){
+    if( (cardMonthInput.value >= 1 && cardMonthInput.value < 12) && cardYearInput.value >= 22 && cardYearInput.value <= 30){
+        let cardExpiryString = cardMonthInput.value + '/' + cardYearInput.value
+        cardExpiry.innerHTML = cardExpiryString
+    }
+}
 
 document.querySelector("#form").addEventListener('submit', function(e){
     // e.preventDefault()

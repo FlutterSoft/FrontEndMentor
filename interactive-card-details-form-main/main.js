0e705b38cdf76558d535580bcb330c97f7e162f6
Sam Brown <jumpbro@hotmail.com>
2022-10-25 08:43:07 +1000

    Validation and better processing of inputs

--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -1,39 +1,58 @@
-const cardNameInput = document.querySelector("#name-input")
-const cardNumberInput = document.querySelector("#number-input")
-const cardMonthInput = document.querySelector("#month-expiry-input")
-const cardYearInput = document.querySelector("#year-expiry-input")
-const cardCvcInput = document.querySelector("#cvc-input")
+const $ = document.querySelector.bind(document)
 
-const cardNumber = document.querySelector("#card-number")
-const cardName = document.querySelector("#card-name")
-const cardExpiry = document.querySelector("#card-expiry")
-const cardCvc = document.querySelector("#card-cvc")
+const cardNameInput = $("#name-input")
+const cardNumberInput = $("#number-input")
+const cardMonthInput = $("#month-expiry-input")
+const cardYearInput = $("#year-expiry-input")
+const cardCvcInput = $("#cvc-input")
 
-document.querySelector("#number-input").addEventListener('change', function(){
-    cardNumber.innerHTML = this.value.slice(0,4) + ' ' + this.value.slice(4,8) + ' ' +
-    this.value.slice(8,12) + ' ' +
-    this.value.slice(12, 16)
+const cardNumber = $("#card-number")
+const cardName = $("#card-name")
+const cardExpiry = $("#card-expiry")
+const cardCvc = $("#card-cvc")
+
+$("#number-input").addEventListener('input', function(){
+    if(this.value.length > 16){
+        this.value = this.value.slice(0, 16)
+    }
+    if(this.value.length == 16){
+        cardNumber.innerHTML = this.value.slice(0,4) + ' ' + this.value.slice(4,8) + ' ' +
+        this.value.slice(8,12) + ' ' +
+        this.value.slice(12, 16)
+    }
 })
 
-document.querySelector("#cvc-input").addEventListener('change', function(){
+$("#cvc-input").addEventListener('input', function(){
+    if(this.value.length > 3){
+        this.value = this.value.slice(0, 3)
+    }
     if(this.value > 0 && this.value < 999 && this.value.length == 3)
     cardCvc.innerHTML = this.value
 })
 
-document.querySelector("#name-input").addEventListener('change', function(){
+$("#name-input").addEventListener('input', function(){
     cardName.innerHTML = this.value.toUpperCase()
 })
-document.querySelector("#month-expiry-input").addEventListener('change', addExpiry)
-document.querySelector("#year-expiry-input").addEventListener('change', addExpiry)
+
+$("#month-expiry-input").addEventListener('input', addExpiry)
+$("#year-expiry-input").addEventListener('input', addExpiry)
+
 function addExpiry(){
-    if( (cardMonthInput.value >= 1 && cardMonthInput.value < 12) && cardYearInput.value >= 22 && cardYearInput.value <= 30){
+    if(this.value.length > 2){
+        this.value = this.value.slice(0, 2)
+    }
+
+    if( (cardMonthInput.value >= 1 && cardMonthInput.value <= 12) && cardYearInput.value >= 22 && cardYearInput.value <= 30){
         let cardExpiryString = cardMonthInput.value + '/' + cardYearInput.value
         cardExpiry.innerHTML = cardExpiryString
     }
 }
 
-document.querySelector("#form").addEventListener('submit', function(e){
-    // e.preventDefault()
-    console.log('yoo')
+$("#form").addEventListener('submit', function(e){
+
+    e.preventDefault()
+    console.log(e)
+
 })
+
 

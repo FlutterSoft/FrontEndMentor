78b7977ae66dbba39511fe807440ee355ea60040
Sam Brown <jumpbro@hotmail.com>
2022-10-30 18:49:37 +1000

    Completed with readme and comments

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -2,18 +2,19 @@ const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 const overlay = document.querySelector(".grey-overlay")
 
-
+// Toggles the active class on the hamburger menu and nav menu (displaying/hiding them)
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
     if(hamburger.classList.contains('active')){
+        // Adds the background overlay (being darkened/grayed)
         overlay.classList.remove('hidden')
         setTimeout(()=>{
             overlay.classList.remove('opaque')
         }, 10)
-
     }
     else{
+        // Removes the background overlay (being darkened/grayed)
         overlay.classList.add('opaque')
         setTimeout(()=>{
             overlay.classList.add('hidden')
@@ -21,12 +22,12 @@ hamburger.addEventListener('click', () => {
     }
 })
 
+// Hides mobile menu when overlay is clicked (so clicking in the background closes the menu)
 overlay.addEventListener('click', ()=>{
     hamburger.click()
-
 })
 
-
+// Adds the ability to hide the menu anytime a navigation link is clicked.
 document.querySelectorAll('.nav-link').forEach(n => {
     n.addEventListener('click', () => {
         hamburger.classList.remove('active')

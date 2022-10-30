1da869f82be91416d552f694a1f832f632a81b32
Sam Brown <jumpbro@hotmail.com>
2022-10-30 12:04:21 +1000

    Mobile view complete

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -1,14 +1,39 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+const overlay = document.querySelector(".grey-overlay")
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
+    if(hamburger.classList.contains('active')){
+        overlay.classList.remove('hidden')
+        setTimeout(()=>{
+            overlay.classList.remove('opaque')
+        }, 10)
+
+    }
+    else{
+        overlay.classList.add('opaque')
+        setTimeout(()=>{
+            overlay.classList.add('hidden')
+        }, 410)
+    }
 })
+
+overlay.addEventListener('click', ()=>{
+    hamburger.click()
+
+})
+
 
 document.querySelectorAll('.nav-link').forEach(n => {
     n.addEventListener('click', () => {
         hamburger.classList.remove('active')
         navMenu.classList.remove('active')
+        overlay.classList.add('opaque')
+        setTimeout(()=>{
+            overlay.classList.add('hidden')
+        }, 410)
     })
 })

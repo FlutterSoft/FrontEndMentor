010684cbf57ec93499004156fb3f8bf3d40f8a98
Sam Brown <jumpbro@hotmail.com>
2022-11-02 08:50:05 +1000

    Bars now have popups on mouseover with correct data from json

--- expenses-chart-component-main/main.js
+++ expenses-chart-component-main/main.js
@@ -1,7 +1,26 @@
+const bars = document.querySelectorAll('.bar')
+const columns = document.querySelectorAll('.chart-column')
+const dailySpendings = [...columns].map(col => col.childNodes[5])
+// console.log(dailySpendings)
+
+bars.forEach(bar => bar.addEventListener('mouseover', function(){
+    console.log(bar.parentNode)
+    bar.parentNode.childNodes[5].style.display = "block"
+}))
+bars.forEach(bar => bar.addEventListener('mouseout', function(){
+    console.log(bar.parentNode)
+    bar.parentNode.childNodes[5].style.display = "none"
+}))
+
+// FETCH DATA
 async function fetchChartData(){
     const chartFetch = await fetch('./data.json')
     const chartData = await chartFetch.json()
-    console.log(chartData)
-}
+    // console.log(chartData)
 
+    for(let i = 0; i < dailySpendings.length; i++){
+        dailySpendings[i].innerHTML = "$" + chartData[i].amount
+    }
+
+}
 fetchChartData()

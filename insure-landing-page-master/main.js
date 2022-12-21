5ce72694e381156549a9b161946b198bb10f579a
Sam Brown <jumpbro@hotmail.com>
2022-12-21 19:43:11 +1000

    Nearly finished

--- insure-landing-page-master/main.js
+++ insure-landing-page-master/main.js
@@ -1,4 +1,3 @@
-
 const $ = document.querySelector.bind( document )
 const hamburger = $('.hamburger')
 const nav = $('.main-nav')
@@ -9,20 +8,23 @@ const navLinks = document.querySelectorAll('.nav-link')
 hamburger.addEventListener('click', toggleNav )
 
 function toggleNav(){
-    if(nav.classList.contains('active')){
-        nav.classList.remove('active')
-        body.classList.remove('position-fixed')
-        hamburger.src=("./images/icon-hamburger.svg")
+    if(window.innerWidth <= 650){
+        if(nav.classList.contains('active')){
+            nav.classList.remove('active')
+            body.classList.remove('position-fixed')
+            hamburger.src=("./images/icon-hamburger.svg")
+        }
+        else{
+            nav.classList.remove('hidden')
+            nav.classList.add('active')
+            body.classList.add('position-fixed')
+            hamburger.src=("./images/icon-close.svg")
+        }
+    }
 
-    }
-    else{
-        nav.classList.remove('hidden')
-        nav.classList.add('active')
-        body.classList.add('position-fixed')
-        hamburger.src=("./images/icon-close.svg")
-    }
 }
 navLinks.forEach(link => {
+
     link.addEventListener('click', toggleNav)
 
 })

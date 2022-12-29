25ad1acf6b2bffc183fc160150b0ffe48ddab73b
Sam Brown <jumpbro@hotmail.com>
2022-12-29 22:08:00 +1000

    All javascript working for selector buttons and data changing

--- time-tracking-dashboard-main/main.js
+++ time-tracking-dashboard-main/main.js
@@ -2,22 +2,53 @@ const cardHeadings = document.querySelectorAll('.card-heading')
 const selectorDaily = document.querySelector('#selector-daily')
 const selectorWeekly = document.querySelector('#selector-weekly ')
 const selectorMonthly = document.querySelector('#selector-monthly')
-// FETCH DATA
-async function fetchData(){
-    const dataFetch = await fetch('./data.json')
-    const data = await dataFetch.json()
-    console.log(data)
-    setHeadings(data)
-}
+const selectors = document.querySelectorAll('.frequency-selector-btn')
+const allHours = document.querySelectorAll('.hours')
+const allPreviousHours = document.querySelectorAll('.previous-hours')
 
-fetchData()
+selectorDaily.addEventListener('click', (e)=> {
+    setHrs('daily', 'Yesterday ')
+    setSelected(e)
+})
+selectorWeekly.addEventListener('click', (e)=> {
+    setHrs('weekly', 'Last Week ')
+    setSelected(e)
+})
+selectorMonthly.addEventListener('click', (e)=> {
+    setHrs('monthly', 'Last Month ')
+    setSelected(e)
+})
 
-function setHeadings(data){
-    data.forEach( (item, index) => {
-        cardHeadings[index].innerHTML=item.title
+async function setHrs(frequency, word){
+    const data = await fetchData()
+    allHours.forEach( (el, index) => {
+        el.innerHTML=data[index].timeframes[frequency].current + 'hrs'
+    })
+    allPreviousHours.forEach( (el, index) => {
+        el.innerHTML=`${word} - ${data[index].timeframes[frequency].previous}hrs`
     })
 }
 
-function setHrs(frequency){
+function setSelected(e){
+    selectors.forEach(selector => {
+        if(e.target != selector){
+            selector.classList.remove('selected')
+        }
+        else{
+            selector.classList.add('selected')
+        }
+    })
+}
 
+// FETCH DATA
+async function fetchData(){
+    try{
+        const response = await fetch('./data.json')
+        const data = await response.json()
+        return data
+    }
+    catch(err){
+        console.log(err)
+        throw err
+    }
 }
